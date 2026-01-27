6d0f3c268bcbc678e6b037ccdba51b8bd6e460f1
Tomone-A <pengyinchize@gmail.com>
2026-01-27 17:50:38 +0900

    AtCoderの練習問題ACを追加

---
+++ AtCoder/Practice/Product.py
@@ -0,0 +1,11 @@
+# ABC086A - Product
+# 問題URL: https://atcoder.jp/contests/abs/tasks/abc086_a
+
+a, b = map(int, input().split())
+
+if (a * b) % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# AC！
