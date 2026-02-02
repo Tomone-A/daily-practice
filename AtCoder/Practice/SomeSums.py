a0ce8b783e280887fae9aab2b1a94731c6947262
Tomone-A <pengyinchize@gmail.com>
2026-02-02 20:15:46 +0900

    AtCoderの練習問題ACを追加

--- AtCoder/Practice/SomeSums.py
+++ AtCoder/Practice/SomeSums.py
@@ -2,33 +2,43 @@
 # 問題URL: https://atcoder.jp/contests/abs/tasks/abc083_b
 
 n, a, b = map(int, input().split())
-# n < 10000
-digits = []
-d5 = 0
-d4 = 0
-d3 = 0
-d2 = 0
-d1 = 0
+
+nums = []
 
 # 各桁ごとにリストに格納する処理
-tmp = n
-d5 = tmp // 10000
-digits.append(d5)
-tmp = tmp - (d5 * 10000)
-d4 = tmp // 1000
-digits.append(d4)
-tmp = tmp - (d4 * 1000)
-d3 = tmp // 100
-digits.append(d3)
-tmp = tmp - (d3 * 100)
-d2 = tmp // 10
-digits.append(d2)
-tmp = tmp - (d2 * 10)
-d1 = tmp // 1
-digits.append(d1)
-tmp = tmp - (d1 * 10)
+# n < 10000 という制約を利用
+def digsplit(x, y, z):
+    tmp = x
+    digits = []
+    d5 = 0
+    d4 = 0
+    d3 = 0
+    d2 = 0
+    d1 = 0
+    s = 0
+    d5 = tmp // 10000
+    digits.append(d5)
+    tmp = tmp - (d5 * 10000)
+    d4 = tmp // 1000
+    digits.append(d4)
+    tmp = tmp - (d4 * 1000)
+    d3 = tmp // 100
+    digits.append(d3)
+    tmp = tmp - (d3 * 100)
+    d2 = tmp // 10
+    digits.append(d2)
+    tmp = tmp - (d2 * 10)
+    d1 = tmp // 1
+    digits.append(d1)
+    tmp = tmp - (d1 * 10)
+    # 「A以上B以下」の処理
+    if y <= sum(digits) <= z :
+        nums.append(x)
 
-# 今日は一旦終了 これを1から順にn以下まで繰り返す処理にして、
-# 各桁の和がA以上B以下のときだけリストに取り出したい。できるかな
+# nまで全部試す
+for i in range(n + 1) :
+    digsplit(i, a, b)
 
-print(digits)
+print(sum(nums))
+
+# AC！
