4a8b00e4ce9ce250e83304468827695f91fc1ed4
Tomone-A <pengyinchize@gmail.com>
2026-02-06 22:50:12 +0900

    AtCoderの練習問題ACを追加

--- AtCoder/Practice/Otoshidama.py
+++ AtCoder/Practice/Otoshidama.py
@@ -1,26 +1,24 @@
 # Otoshidama
 # 問題URL: https://atcoder.jp/contests/abs/tasks/abc085_c
 
-maisu = int(input())
-sum = int(input())
+n, sum = map(int, input().split())
 x = 0
 y = 0
 z = 0
 
-for p in range(maisu + 1):
-    for q in range(maisu + 1):
-        for r in range(maisu + 1):
-            if p*10000 + q*5000 + r*1000 == sum and p+q+r == maisu:
-                x = p
-                y = q
-                z = r
-                break
-            else:
-                x = -1
-                y = -1
-                z = -1
+for p in range(n + 1):
+    for q in range(n + 1 - p):
+        z = n - (p + q)
+        if p * 10000 + q * 5000 + z * 1000 == sum:
+            x = p
+            y = q
+            print(x, y, z)
+            exit()
+        else:
+            x = -1
+            y = -1
+            z = -1
 
 print(x, y, z)
 
-# わからんので明日に持ち越します
-# ヒントほしい
+# AC （Geminiヒント使用）
