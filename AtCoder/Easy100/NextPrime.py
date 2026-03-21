529e5f740f48bccce6a622c468fd5c2394a47d2f
Tomone-A <pengyinchize@gmail.com>
2026-03-21 21:43:04 +0900

    AtCoderの練習問題ACを追加

--- AtCoder/Easy100/NextPrime.py
+++ AtCoder/Easy100/NextPrime.py
@@ -1,3 +1,18 @@
 # Next Prime
 # 問題URL: https://atcoder.jp/contests/abc149/tasks/abc149_c
 
+x = int(input())
+count = 0
+
+def primecheck(num):
+    for i in range(2, num - 1, 1):
+        if x % i == 0:
+            return False
+    return True
+
+while(primecheck(x) == False):
+    x += 1
+
+print(x)
+
+# AC!
