1f1dfdeab09d78d05fba1a7112b9f63ec4b60363
Tomone-A <pengyinchize@gmail.com>
2026-03-27 20:42:29 +0900

    AtCoderの練習問題ACを追加

--- AtCoder/Easy100/CountOrder.py
+++ AtCoder/Easy100/CountOrder.py
@@ -1,16 +1,27 @@
 # Count Order
 # 問題URL: https://atcoder.jp/contests/abc150/tasks/abc150_c
+import itertools
 
-n = int(input())
+n = [ i+1 for i in range(int(input()))]
 p = list(map(int, input().split()))
 q = list(map(int, input().split()))
 
-dict = []
+dict = list(itertools.permutations(n))
+dict = [ list(i) for i in dict ] # 辞書探索用に、タプルからリストに変換
 
-for i in range(n):
-    tmp = []
-    for j in range(n):
-        tmp.append(j+1)
-    dict.append(tmp)
+pnum = 0
+qnum = 0
 
-print(dict)
+for i in range(len(dict)):
+    if dict[i] == p:
+        pnum = i+1
+        if dict[i] == q: # たぶん冗長だけど、pとqが同じだった場合の処理
+            qnum = i+1 
+    elif dict[i] == q:
+        qnum = i+1
+    else:
+        pass
+
+print(abs(pnum - qnum))
+
+# AC!
