5303ddecf712a1efd1fce713bbfd3d8d466db18b
Tomone-A <pengyinchize@gmail.com>
2026-03-30 10:41:23 +0900

    AtCoderの練習問題ACを追加

--- AtCoder/Easy100/CountBalls.py
+++ AtCoder/Easy100/CountBalls.py
@@ -1,3 +1,17 @@
 # Count Balls
 # 問題URL: https://atcoder.jp/contests/abc158/tasks/abc158_b
 
+n, a, b = map(int, input().split())
+
+rep = n // (a + b) # 繰り返しの回数
+amari = n % (a + b)
+ans = 0
+
+if amari <= a:
+    ans = rep * a + amari
+else:
+    ans = rep * a + a
+
+print(ans)
+
+# 一発AC!
