6650e73045875b449ff18989c391355881f2b920
Tomone-A <pengyinchize@gmail.com>
2026-03-31 19:57:59 +0900

    AtCoderの練習問題ACを追加

--- AtCoder/Easy100/754.py
+++ AtCoder/Easy100/754.py
@@ -1,4 +1,17 @@
 # 754
 # 問題URL: https://atcoder.jp/contests/abc114/tasks/abc114_b
 
+s = list(str(input()))
+x = []
 
+for i in range(len(s) - 2):
+    x.append(int(s[i] + s[i+1] + s[i+2]))
+
+x_dif = []
+
+for num in x:    
+    x_dif.append(abs(753 - num))
+
+print(min(x_dif))
+
+# AC!
