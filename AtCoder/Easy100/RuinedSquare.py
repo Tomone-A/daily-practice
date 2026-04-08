528d7b156014dbcb32327065e4617dad3c985305
Tomone-A <pengyinchize@gmail.com>
2026-04-08 09:15:11 +0900

    AtCoderの練習問題ACを追加

--- AtCoder/Easy100/RuinedSquare.py
+++ AtCoder/Easy100/RuinedSquare.py
@@ -1,4 +1,16 @@
 # Ruined Square
 # 問題URL: https://atcoder.jp/contests/abc108/tasks/abc108_b
 
-# わからん
+x1, y1, x2, y2 = map(int, input().split())
+x3, y3, x4, y4 = 0, 0, 0, 0
+gradx = x2 - x1
+grady = y2 - y1
+
+x3 = x2 + (grady * -1)
+y3 = y2 + (gradx)
+x4 = x3 + (gradx * -1)
+y4 = y3 + (grady * -1)
+
+print(x3, y3, x4, y4)
+
+# AC!
