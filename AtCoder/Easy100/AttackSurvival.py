53405d4da71b2d2f36f96499093eb8c1a80f5469
Tomone-A <pengyinchize@gmail.com>
2026-04-17 20:54:28 +0900

    進捗保存

--- AtCoder/Easy100/AttackSurvival.py
+++ AtCoder/Easy100/AttackSurvival.py
@@ -4,3 +4,16 @@
 n, k, q = map(int, input().split())
 correct = [ int(input()) for i in range(q) ]
 
+score = k - q # 1回も回答しなかった場合の最低スコアを出しておく
+
+for i in range(1, n+1, 1):
+    tmp = 0
+    for j in correct:
+        if j == i: # 正解数をポイントに加算し、最低スコアと足す
+            tmp += 1
+    if score + tmp > 0: # 最終的なスコアが0点より高いか判定
+        print('Yes')
+    else:
+        print('No')
+
+# TLE
