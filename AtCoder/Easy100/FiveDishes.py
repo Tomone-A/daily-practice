2cf66aa5f9b1e62733e5913ff631389e35555a38
Tomone-A <pengyinchize@gmail.com>
2026-04-24 09:55:21 +0900

    AtCoderの練習問題ACを追加

--- AtCoder/Easy100/FiveDishes.py
+++ AtCoder/Easy100/FiveDishes.py
@@ -1,3 +1,19 @@
 # Five Dishes
 # 問題URL: https://atcoder.jp/contests/abc123/tasks/abc123_b
+import math
 
+time = 0
+dishes = [int(input()) for i in range(5)]
+last_digit = [(i % 10) for i in dishes]
+last_digit = [i for i in last_digit if i != 0] # 割り切れるものを削除
+last_order = 0
+if last_digit != []: # 割り切れないものが1つでもあれば
+    last_order = 10 - min(last_digit)
+
+for d in dishes:
+    tmp = (math.ceil(d / 10)) * 10 # 下1桁を切り上げ
+    time += tmp
+
+print(time - last_order)
+
+# AC!
